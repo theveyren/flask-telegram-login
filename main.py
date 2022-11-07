b8e41b62c9cc98d75164ca7b851deca9e3871dfd
theveyren <67864293+theveyren@users.noreply.github.com>
2022-11-07 22:20:11 +0500

    Мелкие правки

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from quart import Quart, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Quart(__name__)
-app.config['SECRET_KEY'] = "TELEGRAM-LOGIN"
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "BESTSECRETKEYINTHISWORLD"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-app.jinja_env.globals.update(bot_id = REQUIRED) #Айди бота из токена до знака ":"
-app.jinja_env.globals.update(bot_name = REQUIRED) #Имя вашего бота с приставкой bot
-app.jinja_env.globals.update(domain = REQUIRED) #Домен вашего сайта из /setdomain в BotFather (обычно http://127.0.0.1:5000)
+app.jinja_env.globals.update(BOTID = REQUIRED) #айди бота из токена до знака ":"
+app.jinja_env.globals.update(BOTNAME = REQUIRED) #имя вашего бота с приставкой bot
+app.jinja_env.globals.update(BOTDOMAIN = REQUIRED) #домен вашего сайта из /setdomain в BotFather (обычно http://127.0.0.1:5000)
 
 @app.before_request
-async def make_session_permanent():
+def make_session_permanent():
     session.permanent = True
 
-async def template(tmpl_name, **kwargs):
+def template(tmpl_name, **kwargs):
     telegram = False
     user_id = session.get('user_id')
     username = session.get('name')
@@ -21,7 +21,7 @@ async def template(tmpl_name, **kwargs):
     if user_id:
         telegram = True
 
-    return await render_template(tmpl_name,
+    return render_template(tmpl_name,
                            telegram = telegram,
                            user_id = user_id,
                            name = username,
@@ -29,11 +29,11 @@ async def template(tmpl_name, **kwargs):
                            **kwargs)
 
 @app.route("/")
-async def index():
-    return await template("telegram.html")
+def index():
+    return template("telegram.html")
 
 @app.route("/logout")
-async def logout():
+def logout():
     session.pop("user_id")
     session.pop("name")
     session.pop("photo")
@@ -41,7 +41,7 @@ async def logout():
     return redirect(url_for('index'))
 
 @app.route("/login")
-async def login():
+def login():
     user_id = request.args.get("id")
     first_name = request.args.get("first_name")
     photo_url = request.args.get("photo_url")
@@ -53,4 +53,4 @@ async def login():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run("0.0.0.0", debug = True)
